69a63c61861f333a49b15d9d739d329975450995
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-06-21 21:52:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+
+            do
+            {
+                Console.WriteLine("Informe o Sexo, (F) para Feminino, (M) para Mascolino: ");
+                sexo = char.Parse(Console.ReadLine());
+            }
+            while (sexo != 'M' && sexo != 'F');
+            Console.WriteLine("Sexo {0}", sexo);
+        }
+    }
+}
